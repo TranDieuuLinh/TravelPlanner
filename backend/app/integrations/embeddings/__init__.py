35d09bee9891e2a823e79287fa20d58e7df271c0
VTTr2004 <vttr2004@gmail.com>
2026-08-02 15:32:17 +0700

    Revert "chore: stage planner retrieval modules before merge"
    This reverts commit 7a79bf261610dfb6b2ea3d17c0481c6605329f02.

--- backend/app/integrations/embeddings/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.integrations.embeddings.base import EmbeddingClient
-from app.integrations.embeddings.gemini import GeminiEmbeddingClient
-
-__all__ = ["EmbeddingClient", "GeminiEmbeddingClient"]
