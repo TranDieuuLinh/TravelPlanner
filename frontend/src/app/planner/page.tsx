09ad96357c049aa4962603d113f0eb1a1a923062
loipv212 <lequocdai0303@gmail.com>
2026-08-18 10:42:45 +0700

    sửa UI tab nhóm

--- frontend/src/app/planner/page.tsx
+++ frontend/src/app/planner/page.tsx
@@ -1,6 +1,7 @@
 import "maplibre-gl/dist/maplibre-gl.css";
 import "@/styles/global/explore.css";
 import "@/styles/global/itinerary.css";
+import "@/styles/global/groups.css";
 import "@/features/planner/styles/planner-shell.css";
 
 export { default } from "@/features/planner/components/PlannerPage";
